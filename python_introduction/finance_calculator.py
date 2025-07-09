a379a0bca3c5b85ff8d38d45b5a42a9b402f057e
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-07-09 06:47:32 +0100

    Correction 1

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,12 +1,12 @@
 #User input for financial details
-income = input("Enter your monthly income: ")
-expense = input("Enter your total monthly expenses: ")
+monthly_income = input("Enter your monthly income: ")
+monthly_expenses = input("Enter your total monthly expenses: ")
 
 #calculating monthly savings
-savings = int(income) - int(expense)
+monthly_savings = int(monthly_income) - int(monthly_expenses)
 
 #performing simple interest on savings
-principal = savings * 12
+principal = monthly_savings * 12
 rate = 0.05
 time = 12
 
@@ -15,6 +15,6 @@ interest = principal * rate * time
 #calculating the projected savings after a year
 projected_savings = principal + interest
 
-print(f'Your monthly savings are ${savings}')
+print(f'Your monthly savings are ${monthly_savings}')
 print(f'Projected savings after one year, with interest is: ${projected_savings}')
 

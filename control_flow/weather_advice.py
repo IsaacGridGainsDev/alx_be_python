9960ca466db24dcf588d6f9a58685e59fb30cbaa
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-07-18 06:27:43 +0100

    learning control flow

--- control_flow/weather_advice.py
+++ control_flow/weather_advice.py
@@ -2,12 +2,11 @@
 user_input = input("What's the weather like today? (sunny/rainy/cold)").lower()
 
 #creating a match-case control flow gate
-match user_input:
-	case "sunny":
-		print("Wear a t-shirt and sunglasses.")
-	case "rainy":
-		print("Don't forget your umbrella and a raincoat.")
-	case "cold":
-		print("Make sure to wear a warm coat and a scarf.")
-	case _:
-		print("Sorry, I don't have recommendations for this weather.")
+if user_input == "sunny":
+	print("Wear a t-shirt and sunglasses.")
+elif user_input == "rainy":
+	print("Don't forget your umbrella and a raincoat.")
+elif "cold":
+	print("Make sure to wear a warm coat and a scarf.")
+else:
+	print("Sorry, I don't have recommendations for this weather.")

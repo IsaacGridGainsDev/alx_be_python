cb764a1fb1451f021d1ca43bf1ea45ebcf8a55bd
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-07-18 06:35:58 +0100

    renamed user_input variable to weather

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,12 @@
 #Defining a list of predefined weather conditions
-user_input = input("What's the weather like today? (sunny/rainy/cold):")
+weather = input("What's the weather like today? (sunny/rainy/cold):")
 
 #creating a match-case control flow gate
-if user_input == "sunny":
+if weather == "sunny":
 	print("Wear a t-shirt and sunglasses.")
-elif user_input == "rainy":
+elif weather == "rainy":
 	print("Don't forget your umbrella and a raincoat.")
-elif "cold":
+elif weather == "cold":
 	print("Make sure to wear a warm coat and a scarf.")
 else:
 	print("Sorry, I don't have recommendations for this weather.")

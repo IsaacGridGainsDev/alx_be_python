24691cbc9209206f8bb67dd475c72e66884a059c
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-07-22 08:18:00 +0100

    area function

---
+++ challenges/calcluate_area.py
@@ -0,0 +1,8 @@
+def area(length=0, breadth=1):
+	dim_l = input("Enter the length: ")
+	dim_w = input("Enter the width: ")
+	length = int(dim_l)
+	breadth = int(dim_w)
+	area = length * breadth
+	print(f" The area is {area}")   
+area()

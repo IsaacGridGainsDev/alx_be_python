e24fb2d20a1f4952ee728ff9a9f2a51f8ee31912
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-07-22 08:57:21 +0100

    Set operations on random set of numbers

---
+++ challenges/accessing_lists.py
@@ -0,0 +1,20 @@
+#exercise one on lists
+fruits = ["mango", "cashew", "banana", "apple", "carrots", "date"]
+
+print(fruits[1])
+
+#exercise 2
+Fav_book = {
+	'title': 'The richest man in Babylon',
+	'author': 'George S. Carlson',
+	'genre': 'Self-Improvement'
+	}
+#exercise 3
+import random
+
+numbers = []
+for interval in range(0,10):
+	number = random.randint(1, 20)
+	numbers.append(number)
+print(set(numbers))
+

a1a7f3fe13ab43b747b3e8b0bf89e6a7c025ad6b
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-07-24 07:20:07 +0100

    using modules to build

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,26 @@
+def perform_operation(num1: float, num2: float, operation: str) -> float:
+	match operation:
+		case '+' | 'add':
+			answer = num1 + num2
+			return answer
+		case '-' | 'subtract':
+			if num1 > num2:
+				answer = num1 - num2
+				return answer
+			else:
+				answer = num2 - num1
+				return answer
+		case '*' | 'multiply':
+			answer = num1 * num2
+			return answer
+		case '/' | 'divide':
+			try:
+				answer = num1 / num2
+			except ZeroDivisionError:
+				print("cannot divide by zero! ")
+				return float("inf")
+			else:
+				return answer
+		case _:
+			print("invalid operation")
+			return float("nan")

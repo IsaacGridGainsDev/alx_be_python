9c4392d734fe0cef1e9a24d1f8e5d978228d9c0e
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-07-24 07:55:36 +0100

    Checker, checkergit add .git add .!

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -6,7 +6,7 @@ def display_menu():
 	print("4. Exit")
 
 def main():
-	items = []
+	shopping_list = []
 	while True:
 		display_menu()
 		choice = input("Enter your choice: ")
@@ -15,23 +15,23 @@ def main():
 		if choice == '1':
 		# Prompt for and add an item
 			item = input("Enter in the item: ")
-			print(f"{item} has been added into ", items)
-			items.append(item)
+			print(f"{item} has been added into Shopping List")
+			shopping_list.append(item)
 			pass
 
 		elif choice == '2':
 			# Prompt for and remove an item
 			item = input("Enter in the item: ")
-			if item in items:
-				items.remove(item)
-				print(f"{item} has been removed from ", items)
+			if item in shopping_list:
+				shopping_list.remove(item)
+				print(f"{item} has been removed from Shopping List")
 			else:
 				print(f"{item} not found!")
 				pass
 		
 		elif choice == '3':
 			# Display the shopping list
-			print(f"Shopping List:{items}")
+			print(f"Shopping List:{shopping_list}")
 			pass
 
 		elif choice == '4':

f9d52e4ddd849ffbc27a5058e5e5aa56e76a7b08
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-07-24 07:57:01 +0100

    Checker, checkergit add .git add .!

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,14 +14,14 @@ def main():
 
 		if choice == '1':
 		# Prompt for and add an item
-			item = input("Enter in the item: ")
+			item = input("Enter the item to add: ")
 			print(f"{item} has been added into Shopping List")
 			shopping_list.append(item)
 			pass
 
 		elif choice == '2':
 			# Prompt for and remove an item
-			item = input("Enter in the item: ")
+			item = input("Enter the item to remove: ")
 			if item in shopping_list:
 				shopping_list.remove(item)
 				print(f"{item} has been removed from Shopping List")

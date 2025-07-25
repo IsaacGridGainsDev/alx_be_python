744d7e4e702b8e3c30a300bf3726bb2d502368a9
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-07-25 06:33:02 +0100

    increading logical applications

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,23 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_to_celsius(fahrenheit):
+	temp = fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
+	return temp
+def convert_to_fahrenheit(celsius):
+	temp = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
+	return temp
+
+def main():
+	user_temp = float(input("Enter the temperature to convert: "))
+	temp_format = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+	match temp_format:
+		case 'C' | 'c':
+			converted_temp = convert_to_fahrenheit(user_temp)
+			print(f"{user_temp:.1f}C is {converted_temp:.4f}F")
+		case 'F'| 'f':
+			converted_temp = convert_to_celsius(user_temp)
+			print(f"{user_temp:.1f}F is {converted_temp:.4f}C")
+		case _:
+			print("invalid input")
+main()

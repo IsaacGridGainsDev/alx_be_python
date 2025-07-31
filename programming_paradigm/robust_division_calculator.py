c50c26d8941fbc2183afbbc2406b97f3d753e1d6
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-07-31 06:54:20 +0100

    Exercise 2

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,11 @@
+def safe_divide(numerator, denominator):
+	try:
+		try:
+			result = float(numerator) / float(denominator)
+		except ValueError:
+			return "Error: Please enter numeric values only."
+		else:
+			return f"The result of the division is {result:.1f}"
+	except ZeroDivisionError:
+		return "Error: Cannot divide by zero"
+	

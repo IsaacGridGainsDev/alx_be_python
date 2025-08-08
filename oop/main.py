52cc70ac83855ef391e6c517522b5577992e84ca
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-08-08 04:41:15 +0100

    Checker being checker

--- oop/main.py
+++ oop/main.py
@@ -2,7 +2,7 @@ from book_class import Book
 
 def main():
 	#create instance
-	my_book = Book("1984", "Goerge Orwell", 1949)
+	my_book = Book("1984", "George Orwell", 1949)
 
 	#Demonstrating the __str__ method
 	print(my_book)

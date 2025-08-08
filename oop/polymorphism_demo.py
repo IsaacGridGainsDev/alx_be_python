74411d62195e1e4324d29376025c581539e436b8
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-08-08 05:25:37 +0100

    Project on polymorphism

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,22 @@
+import math
+
+class Shape:
+	def __init__(self):
+		pass
+	def area(self):
+		#area = length * width
+		return NotImplementedError
+class Rectangle(Shape):
+	def __init__(self, length, width):
+		self.length = length
+		self.width = width
+	def area(self):
+		area = self.length * self.width
+		return area
+class Circle(Shape):
+	def __init__(self, radius):
+		self.radius = radius
+
+	def area(self):
+		area = math.pi * (self.radius ** 2)
+		return area

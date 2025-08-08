250f92e21267a55f349e52f8298fcb8fdce5c346
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-08-08 05:34:01 +0100

    Understanding decorators

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,13 @@
+class Calculator:
+	calculation_type = "Arithmetic Operations"
+	def __init__(self):
+		pass
+	@staticmethod
+	def add(a, b):
+		result = a+b
+		return result
+	
+	@classmethod
+	def multiply(cls, a, b):
+		print(f"Calculation type: {cls.calculation_type}")
+		return a * b

07fdd0ad340a6777f934724248d006a3393686af
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-08-14 04:55:22 +0100

    Fixed my bad SQL code and added some maipulation

--- database_design/CRUD.sql
+++ database_design/CRUD.sql
@@ -17,3 +17,20 @@ CREATE TABLE Orders (
 	Total INT,
 	FOREIGN KEY (StudentID) REFERENCES Students(StudentID) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+INSERT INTO Students VALUES (1, 'Jacob', 'Millins', 'jake@ea.org.us', '2021-05-04'), (2, 'Favor', 'Amarachi', 'favamarachi@ea.org.ng', '2020-09-09'), (3, 'Abubakar', 'Suleiman', 'abuleiman@ea.org.ng', '2020-09-09');
+
+INSERT INTO Orders VALUES (1, 1, '2025-08-14', 300), (2, 2, '2025-08-14', 240), (3, 3, '2025-08-14', 990);
+
+SELECT * FROM Orders;
+
+SELECT
+	S.FirstName,
+	S.LastName,
+	S.Email,
+	O.Total
+FROM Students AS S
+JOIN Orders AS O ON O.StudentID = S.StudentID
+WHERE S.FirstName = 'Jacob'; 
+
+
